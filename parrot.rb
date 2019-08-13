2c363f16a67c3a29e1b97db701b0cd2fab6537d7
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-13 04:34:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot("string","Squawk!")
-  puts ("#{string}") + ("Squawk!")
-  puts "#{string}"
+def parrot("string" + "Squawk!")
+  puts ("Hello" + parrot)
 end
 
-p parrot
+puts parrot
